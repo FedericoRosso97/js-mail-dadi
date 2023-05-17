11ede0332921dd9e2aaa7b870b84928e7e52ee70
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-17 15:50:47 +0200

    stabilisco i parametri di vittoria

--- node.js
+++ node.js
@@ -1,5 +1,11 @@
 const player=Math.floor(Math.random()*6)+1;
-console.log(player);
+console.log(player+' player');
 
 const pc=Math.floor(Math.random()*6)+1;
-console.log(pc);
+console.log(pc+' pc');
+
+if(player>pc){
+    console.log('HAI VINTOOO!')
+}else if(pc>player){
+    console.log('HAI PERSO! :(')
+}
